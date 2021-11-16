eb17aedfbe16aa8f3d7b37494400ca05e17a960d
agmaksim <perfie8@yandex.ru>
2021-11-16 18:00:06 +0500

    letsgo

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -57,10 +57,9 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    # Устанавливаем срок жизни токена
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-   'AUTH_HEADER_TYPES': ('Bearer',),
-} 
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,4 +1,4 @@
-from api.views import CommentViewSet, GroupViewSet, PostViewSet, UserViewSet
+from api.views import CommentViewSet, GroupViewSet, PostViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
